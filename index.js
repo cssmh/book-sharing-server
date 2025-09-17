2bd79ee06a7c570840563efb128995ac4ecb6b8f
Momen <mominitmbstu@gmail.com>
2025-09-17 21:48:52 +0600

    updated git connection

--- index.js
+++ index.js
@@ -21,16 +21,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(router);
 
-// async function run() {
-//   try {
-//     await client.db("admin").command({ ping: 1 });
-// console.log("Pinged your deployment. Successfully connected to MongoDB!");
-//   } finally {
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 app.get("/", (req, res) => {
   res.send("Share Your Favorite Books");
 });
